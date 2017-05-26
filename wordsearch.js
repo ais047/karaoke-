0a460c20660249d7cfcf0b0bf326db6ec9ef3afa
ais047 <hursungwoo@yahoo.com>
2017-05-25 19:17:01 -0700

    first

---
+++ wordsearch.js
@@ -0,0 +1,8 @@
+var text = "[00:22.31] Take me down to the paradise city [00:25.01] Where the grass is green [00:27.36] And the girls are pretty [00:30.07] Take me home"
+var textsplit = text.split('[');
+//first 8 are number, 9= ], 10 = " "
+textsplit = textsplit.splice(1);
+
+var timestamps = [];
+var lyrics = [];
+console.log(textsplit);
