6db248a523a3c696bb178b60d8000138ea1d0927
Omnomon <jessselu@gmail.com>
2017-05-29 20:56:43 -0700

    test spotify authentication

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,4 @@
-var searchQuery = "queen"
+/*var searchQuery = "queen"
 
 
 var spotifyURL = "https://api.spotify.com/v1/search?q=" + searchQuery + "&type=artist&limit=1"
@@ -12,7 +12,7 @@ $.ajax({
 })
 
 
-
+*/
 //search list?
 var searchfor = ["q_artist", "q_track"];
 function grabSongAndArtist(song, artist) {
---
+++ assets/javascript/singalong.js
@@ -0,0 +1,19 @@
+var searchQuery = "queen"
+
+
+var spotifyURL = "https://api.spotify.com/v1/search?q=" + searchQuery + "&type=artist&limit=1"
+var spotifyKey = "b133885956ff4364a68a1ca5b4870d83";
+console.log(spotifyURL)
+var client_id = "b133885956ff4364a68a1ca5b4870d83";
+var redirect_uri = "file:///C:/Users/Jesse/Desktop/code/karaoke-/index.html"
+var authorizeURL = "https://accounts.spotify.com/authorize?client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&response_type=token"
+
+$(document).on("click", ".testButton", function() {
+	console.log("test")
+    $.ajax({
+        url: authorizeURL,
+        method: "GET"
+    }).done(function(response) {
+        console.log(response)
+    })
+})
